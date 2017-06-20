8baf644e18d75bfa8cd76ba453756639195ecdea
Wouter Remijn <wouter.remijn@gmail.com>
2017-06-20 11:31:26 +0200

    Collisstion test with grid

--- WebContent/javascript/game.js
+++ WebContent/javascript/game.js
@@ -1,26 +1,70 @@
-function createBlock(){
-	var obj = document.getElementById("00");
-	obj.style.background = 'red';
+var grid = [];
+var width = 10;
+var height = 20;
+
+init();
+
+function init() {
+	createGrid();
+	dropBlock();
 }
 
-var x = 0;
-var y = 0;
-
-createBlock();
-fall();
-setInterval(fall, 25);
-
-
-function fall(){	
-	var obj = document.getElementById("" + x + y);	
-	obj.style.background = 'grey';
-	obj = document.getElementById("" + ++x + y);
-	obj.style.background = 'red';
-	if(x == 19) {		
-		x = 0;
-		y = 0;
-		obj.style.background = 'grey';
-		createBlock();		
+function createGrid(){
+	for(var i = 0; i < height; i++) {
+		var row = [];
+		for(var j = 0; j < width; j++) {
+			row.push(false);
+		}
+		grid.push(row);
 	}
+	console.log(grid);
 }
 
+function pos(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+function setColor(pos, color){
+	document.getElementById("" + pos.y + pos.x).style.background = color;
+}
+
+function createBlock() {
+	var position = new pos(4, 0);	
+	var block = {
+			position,
+			color: "red"
+	}
+	setColor(position, block.color);
+	return block;
+}
+
+function canMove(block){
+	curPos = block.position;
+	nextPos = new pos(curPos.x, curPos.y +1);
+	
+	
+	
+	
+	if(grid[nextPos.x][nextPos.y] || nextPos.y == height){
+		grid[curPos.x][curPos.y] = true;
+		return false;
+	}
+	
+	setColor(curPos, "gray")
+	setColor(nextPos, "red");
+	
+	block.position.y++;
+	
+	return true;
+}
+
+function dropBlock() {
+	this.block = createBlock();
+	this.interval = setInterval(function() {
+		if(!canMove(block)) {
+			clearInterval(interval);
+			dropBlock();
+		}
+	}, 50)
+}
